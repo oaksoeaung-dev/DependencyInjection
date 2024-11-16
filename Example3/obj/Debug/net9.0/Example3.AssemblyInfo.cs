0f074c84eb6b4fd507d5af64577e89935a47f921
oaksoeaung-dev <oaksoeaung.dev@gmail.com>
2024-11-16 10:40:51 +0630

    Relocate folder

--- Example3/obj/Debug/net9.0/Example3.AssemblyInfo.cs
+++ Example3/obj/Debug/net9.0/Example3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Example3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+606b6d25a48a4e4f93a7fc5a2272ee239a6033ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Example3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Example3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
